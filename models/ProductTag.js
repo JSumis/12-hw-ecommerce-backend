dd08090248bc4588ac83968ea73b22d1e90d7cf9
Joseph Sumis <Jsumis@comcast.net>
2021-08-31 14:23:18 -0500

    finished models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,29 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // need to reference the Product model's id
+    product_id: { type: DataTypes.INTEGER },
+    // need to reference the Tag models id
+    tag_id: { type: DataTypes.INTEGER },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
